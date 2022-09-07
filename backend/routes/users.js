357c241e566fc82c60cef01d25fafc61f1472de6
SIMS-B <22100128@lums.edu.pk>
2022-09-07 12:34:46 +0500

    completes functionality, sends response with required fields and has jwt authorization implemented as well

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,9 @@ const router = express.Router();
 // importing model for this route
 const users = require('../models/users');  
 
+// importing middleware
+const auth = require("../middleware/auth");
+
 // db connection test script 
 const reading = async () => {
     const fetchAll = await users.query(); 
@@ -16,18 +19,46 @@ const relationalReading = async () => {
 
 // ROUTES
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
+    
+    // req.user contains {is_admin, id}
+
     try
     {
-        console.log("sample users call");
-        // db connection test function
-        await reading();
-        await relationalReading(); 
+        const userId = req.user.id.toString();
+        const userDetails = await users.query()
+                                            .select('cnic', 'email', 'phone_number', 'registration_date')
+                                            .where('id', '=', userId);
+
+        res.status(200).send(userDetails);
     } 
     catch (err)
     {
         console.log(err);   // remove this print
     }
+
+
+});
+
+router.put('/', auth, async (req, res) => {
+    
+    // req.user contains {is_admin, id}
+
+    try
+    {
+        const userId = req.user.id.toString();
+        const userDetails = await users.query()
+                                            .select('cnic', 'email', 'phone_number', 'registration_date')
+                                            .where('id', '=', userId);
+
+        res.status(200).send(userDetails);
+    } 
+    catch (err)
+    {
+        console.log(err);   // remove this print
+    }
+
+
 });
 
 module.exports = router;
