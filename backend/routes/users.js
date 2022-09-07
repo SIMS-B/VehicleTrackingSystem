15a2772ae40bb874b44f88df02ace4e86f7b81ce
SIMS-B <22100128@lums.edu.pk>
2022-09-07 12:40:32 +0500

    contains functional functionality for this feature

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -7,40 +7,9 @@ const users = require('../models/users');
 // importing middleware
 const auth = require("../middleware/auth");
 
-// db connection test script 
-const reading = async () => {
-    const fetchAll = await users.query(); 
-    console.log(fetchAll);
-}
-const relationalReading = async () => {
-    const fetchOrderForThisCNIC = await users.relatedQuery('Order').for('1');
-    console.log(fetchOrderForThisCNIC);
-}
-
 // ROUTES
 
 router.get('/', auth, async (req, res) => {
-    
-    // req.user contains {is_admin, id}
-
-    try
-    {
-        const userId = req.user.id.toString();
-        const userDetails = await users.query()
-                                            .select('cnic', 'email', 'phone_number', 'registration_date')
-                                            .where('id', '=', userId);
-
-        res.status(200).send(userDetails);
-    } 
-    catch (err)
-    {
-        console.log(err);   // remove this print
-    }
-
-
-});
-
-router.put('/', auth, async (req, res) => {
     
     // req.user contains {is_admin, id}
 
