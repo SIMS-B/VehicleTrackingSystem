5c0b4a3e74245ebc5dff45c73ba358c4a1cec1e1
Amaan Khan <amaankhan@gosaas.io>
2022-09-08 15:37:40 +0500

    fetches all configurationd for admin

--- backend/routes/configurations.js
+++ backend/routes/configurations.js
@@ -8,15 +8,27 @@ const auth = require('../middleware/auth');
 
 // db connection test script 
 const reading = async () => {
-    const fetchAll = await Configurations.query();
+    const fetchAll = await Configurations.query().select('po_reception', 'factory_floor', 'vin', 'chassis', 'ready_to_ship', 'arrival_at_vendor')
+    .where('id', '=', 1);
     return fetchAll;
 }
 
 // ROUTES
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
     try
     {
+        const isAdmin = req.user.is_admin;
+        if(isAdmin)
+        {
+        console.log('sample configurations call');
+        const defaultConfigs = await reading();
+        res.status(200).send(defaultConfigs[0]);
+        }
+        else
+        {
+            res.status(403).send("You cannot access this page.");
+        }
         
     } 
     catch (err)
