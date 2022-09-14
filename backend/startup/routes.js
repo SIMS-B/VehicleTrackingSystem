4becdf28276c37ad22685d1d356dc0d48ef23074
SIMS-B <22100128@lums.edu.pk>
2022-09-14 19:19:26 +0500

    swagger refactoring done

--- backend/startup/routes.js
+++ backend/startup/routes.js
@@ -5,11 +5,17 @@ const users = require('../routes/users');
 const orders = require('../routes/orders');
 const configurations = require('../routes/configurations');
 
+// importing swaggerUI and its specs
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpecs = require('./swagger');
+
 module.exports = function(app) {
   // setting up routes to be used against each API request
   app.use(express.json());
   app.use('/api/users', users);
   app.use('/api/orders', orders);
   app.use('/api/configurations', configurations);
+  
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 };
 
